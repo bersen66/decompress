0e2ccb5638ec7df69436838003f1ee611e3dccc4
ortur <orturb@yandex.ru>
2023-10-10 18:00:06 +0300

    Feat(dc): Implemented and tested Decompressing

--- decompress.go
+++ decompress.go
@@ -1,5 +1,84 @@
 package decompress
 
+import (
+	"fmt"
+	"strings"
+)
+
+var numbers = map[rune]int{
+	'0': 0,
+	'1': 1,
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+}
+
+func isDigit(val rune) bool {
+	_, ok := numbers[val]
+	return ok
+}
+
+func digitOf(val rune) int {
+	res, _ := numbers[val]
+	return res
+}
+
 func Decompress(src string) (string, error) {
-	return "aoaoa", nil
+	repr := []rune(src)
+	var sb strings.Builder
+
+	var count = 0
+	var lastRune rune
+	var firstLetter = true
+
+	for i := 0; i < len(repr); {
+
+		if repr[i] == '\\' {
+			if !firstLetter {
+				sb.WriteRune(lastRune)
+				i++
+			} else {
+				firstLetter = false
+			}
+			lastRune = repr[i]
+			i++
+			continue
+		}
+
+		if !isDigit(repr[i]) {
+			if !firstLetter {
+				sb.WriteRune(lastRune)
+			} else {
+				firstLetter = false
+			}
+			lastRune = repr[i]
+			i++
+			continue
+		}
+
+		for i < len(repr) && isDigit(repr[i]) {
+			count = count*10 + digitOf(repr[i])
+			i++
+		}
+
+		if firstLetter {
+			return "", fmt.Errorf("Expected char before number at %v", i)
+		} else {
+			for count > 0 {
+				sb.WriteRune(lastRune)
+				count--
+			}
+			firstLetter = true
+		}
+	}
+
+	if !firstLetter {
+		sb.WriteRune(lastRune)
+	}
+	return sb.String(), nil
 }
--- tests/decompress_test.go
+++ tests/decompress_test.go
@@ -4,8 +4,46 @@ import (
 	"testing"
 
 	dc "github.com/bersen66/decompress"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestDecompress(t *testing.T) {
-	dc.Decompress("It works")
+func TestDecompressFromCondition1(t *testing.T) {
+	result, err := dc.Decompress("a4bc2d5e")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, "aaaabccddddde", result)
+}
+
+func TestDecompressFromCondition2(t *testing.T) {
+	result, err := dc.Decompress("abcd")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, "abcd", result)
+}
+
+func TestDecompressFromCondition3(t *testing.T) {
+	_, err := dc.Decompress("45")
+	assert.NotEqual(t, nil, err)
+}
+
+func TestDecompressFromCondition4(t *testing.T) {
+	result, err := dc.Decompress("")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "", result)
+}
+
+func TestEscape1(t *testing.T) {
+	result, err := dc.Decompress("qwe\\4\\5")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, "qwe45", result)
+}
+
+func TestEscape2(t *testing.T) {
+	result, err := dc.Decompress("qwe\\45")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, "qwe44444", result)
+}
+
+func TestEscape3(t *testing.T) {
+	result, err := dc.Decompress("qwe\\\\5")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, "qwe\\\\\\\\\\", result)
 }
